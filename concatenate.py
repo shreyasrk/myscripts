01d8e85f5a16150c2f65f59202ae75f8d1e62084
shreyasrk <shrykulk.rk85@gmail.com>
2014-05-22 11:55:31 +0530

    Edited Concatenate.py

--- concatenate.py
+++ concatenate.py
@@ -1,6 +1,10 @@
 def main():
     strings = ['hello', 'I', 'am', 'none-da']
-    return strings[0] + strings[1] + strings[2] + strings[3]
+    result = ''
+    for str in strings:
+	result += ' ' + str
+
+    return result
 
 if __name__ == '__main__':
     print main()
